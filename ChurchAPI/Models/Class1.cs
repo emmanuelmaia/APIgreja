8e1d8279607d2ea3da32859b814e72799e64e9c9
Emmanuel Nobrega <emmanuelsmaia@gmail.com>
2022-11-05 02:15:40 -0300

    added functioning prototype

---
+++ ChurchAPI/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using ChurchAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ChurchApi.Models
+{
+    public class MemberContext : DbContext
+    {
+        public MemberContext(DbContextOptions<MemberContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Member> Members { get; set; } = null!;
+    }
+}
